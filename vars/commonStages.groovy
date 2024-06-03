3882fafc63b7a2cc4bb42e3d473ecf31f614b0cd
Ayman Khalil <ayman.khalil33@gmail.com>
2024-06-03 16:42:52 +0300

    return stage

--- vars/commonStages.groovy
+++ vars/commonStages.groovy
@@ -1,12 +1,14 @@
 def lint() {
-    stage("Lint") {
-        steps {
-            echo "Linting..."
-            sh 'rm -f flake8.out pylint.out'
-            sh '${BUILD_VENV}/flake8 --exit-zero --doctests --output-file ./flake8.out'
-            sh '${AIRFLOW_BUILD_VENV}/pylint dags --exit-zero -f parseable > ./pylint.out'
-            sh '${BUILD_VENV}/pylint scripts --exit-zero -f parseable >> ./pylint.out'
-            sh '${BUILD_VENV}/pylint rocks_extension --exit-zero -f parseable >> ./pylint.out'
+    return {
+        stage("Lint") {
+            steps {
+                echo "Linting..."
+                sh 'rm -f flake8.out pylint.out'
+                sh '${BUILD_VENV}/flake8 --exit-zero --doctests --output-file ./flake8.out'
+                sh '${AIRFLOW_BUILD_VENV}/pylint dags --exit-zero -f parseable > ./pylint.out'
+                sh '${BUILD_VENV}/pylint scripts --exit-zero -f parseable >> ./pylint.out'
+                sh '${BUILD_VENV}/pylint rocks_extension --exit-zero -f parseable >> ./pylint.out'
+            }
         }
     }
 }
