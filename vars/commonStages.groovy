17145753d54bcbd116a95a5467f555cd506bcd58
Ayman Khalil <ayman.khalil33@gmail.com>
2024-06-03 16:58:36 +0300

    lint

--- vars/commonStages.groovy
+++ vars/commonStages.groovy
@@ -1,3 +1,12 @@
 def python_version() {
     sh 'python --version'
 }
+
+def lint() {
+    echo "Linting..."
+    sh 'rm -f flake8.out pylint.out'
+    sh '${BUILD_VENV}/flake8 --exit-zero --doctests --output-file ./flake8.out'
+    sh '${AIRFLOW_BUILD_VENV}/pylint dags --exit-zero -f parseable > ./pylint.out'
+    sh '${BUILD_VENV}/pylint scripts --exit-zero -f parseable >> ./pylint.out'
+    sh '${BUILD_VENV}/pylint rocks_extension --exit-zero -f parseable >> ./pylint.out'
+}
